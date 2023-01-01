1aa490f10e30e5911f8a558ba0cf53811790bb07
igor2000xp <igorring@gmail.com>
2023-01-01 21:51:43 +0100

    feat: create app.component.css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,5 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'mw-app',
   templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
 })
+
 export class AppComponent {}
