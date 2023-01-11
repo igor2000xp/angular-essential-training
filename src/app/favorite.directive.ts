d35982c86bafd9acea5cb68a79b0dafd56e46677
igor2000xp <igorring@gmail.com>
2023-01-11 01:54:57 +0100

    feat: correct linting

--- src/app/favorite.directive.ts
+++ src/app/favorite.directive.ts
@@ -5,7 +5,8 @@ import { Directive, HostBinding, Input } from '@angular/core';
 })
 export class FavoriteDirective {
   @HostBinding('class.is-favorite') isFavorite = true;
-  @Input() set mwFavorite(value) {
+  @Input()
+  set mwFavorite(value) {
     this.isFavorite = value;
   }
 }
